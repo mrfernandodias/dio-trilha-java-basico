a348492551742f4bec1b4c2b10048b6b653e039d
Fernando Dias <mrfernandodias@gmail.com>
2025-10-19 13:10:54 -0300

    cria aplicação ContaTerminal para cadastro de conta bancária
    - Entrada de dados via terminal (agência, número, cliente, saldo)
    - Formatação de saída usando concatenação com concat()
    - Atende todos os requisitos do desafio DIO Trilha Java Básico

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -2,6 +2,32 @@ import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da Agência !");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da conta !");
+        int numero = scanner.nextInt();
+        scanner.nextLine(); // Consumir a quebra de linha restante
+
+        System.out.println("Por favor, digite o nome do cliente !");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o saldo !");
+        double saldo = scanner.nextDouble();
+
+        String mensagem = "Olá ".concat(nomeCliente)
+                .concat(", obrigado por criar uma conta em nosso banco, sua agência é ")
+                .concat(agencia)
+                .concat(", conta ")
+                .concat(String.valueOf(numero))
+                .concat(" e seu saldo ")
+                .concat(String.valueOf(saldo))
+                .concat(" já está disponível para saque");
+
+        System.out.println(mensagem);
+
+        scanner.close();
     }
 }
